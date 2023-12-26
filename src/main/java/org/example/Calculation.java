3cf29f900d4503fa8e82e5c83f498125c8048b4a
kamaleshbala01 <kamalesravi54@gmail.com>
2023-12-26 21:15:45 +0530

    init commit

--- src/main/java/org/example/Calculation.java
+++ src/main/java/org/example/Calculation.java
@@ -1,5 +1,61 @@
 package org.example;
 
-public class Calculation {
+import java.util.Scanner;
 
+public class Calculation {
+    static Scanner scanner = new Scanner(System.in);
+    static  boolean flag = true;
+       public static void start(){
+           int count = 0;
+           while(flag){
+               System.out.print("\n1 for lap\n2 for pause\n3 for exit\nEnter your choice : ");
+               int choice = scanner.nextInt();
+               switch (choice){
+                   case 1:
+                           System.out.println("lap : "+count++);
+                           Data.setEndTime(System.currentTimeMillis());
+                           Data.setMilliseconds(Data.getMilliseconds()+Data.getEndTime()-Data.getStartTime());
+                           getCalculation();
+                           Data.setStartTime(System.currentTimeMillis());
+                           break;
+                   case 2: System.out.println("\nYou Paused the stop Watch.....!");
+                           Data.setEndTime(System.currentTimeMillis());
+                           Data.setMilliseconds(Data.getMilliseconds()+Data.getEndTime()-Data.getStartTime());
+                           getCalculation();
+                           pause();
+                           break;
+                   case 3: flag = false;
+                           Data.setStart(false);
+                           break;
+               }
+           }
+       }
+       public static void pause(){
+           while(flag) {
+               System.out.print("1 for resume\n2 for Reset\n3 for exit\nEnter your choice : ");
+               int choice = scanner.nextInt();
+               switch (choice){
+                   case 1: System.out.println("\nYou Resume the stop watch ....!");
+                           Data.setStartTime(System.currentTimeMillis());
+                           start();
+                           break;
+                   case 2: System.out.println("\nYou Reset the Stop Watch....!");
+                           Data.setMilliseconds(0);
+                           flag = false;
+                           break;
+                   case 3: flag = false;
+                           Data.setStart(false);
+                           break;
+               }
+           }
+       }
+       public static void getCalculation(){
+           long milliseconds = Data.getMilliseconds();
+           long hours = milliseconds/(60*60*1000);
+           long minutes =  (milliseconds%(60*60*1000))/(1000*60);
+           long seconds =  (milliseconds%(1000*60))/1000;
+           System.out.println("-----------------------------------------------------------------------------");
+           System.out.println(hours+" : "+minutes+" : "+seconds+" : "+ milliseconds%1000);
+           System.out.println("-----------------------------------------------------------------------------");
+       }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
-
+        Data.setStart(true);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Welcome to KB stopWatch.....!");
+        System.out.println("-----------------------------------------------------------------------------");
+        while(Data.isStart()){
+            System.out.print("\n1 to Start\n2 to exit\nEnter the choice : ");
+            int choice = scanner.nextInt();
+            switch(choice){
+                case 1 : Data.setStartTime(System.currentTimeMillis());
+                         Calculation.start();
+                         break;
+                case 2 : Data.setStart(false);
+                         break;
+            }
+        }
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Thanks for using KB StopWatch....!");
+        System.out.println("-----------------------------------------------------------------------------");
     }
 }
