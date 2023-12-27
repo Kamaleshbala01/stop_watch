b155bbc0104e31a25aaad2c3cf9aa644f0e8618e
kamaleshbala01 <kamalesravi54@gmail.com>
2023-12-27 12:47:32 +0530

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,32 @@
 package org.example;
 
+import java.util.Collection;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Data.setStart(true);
         Scanner scanner = new Scanner(System.in);
+        Data.setStart(true);
         System.out.println("-----------------------------------------------------------------------------");
         System.out.println("Welcome to KB stopWatch.....!");
         System.out.println("-----------------------------------------------------------------------------");
         while(Data.isStart()){
+            Calculation calculation = new Calculation(scanner);
             System.out.print("\n1 to Start\n2 to exit\nEnter the choice : ");
             int choice = scanner.nextInt();
             switch(choice){
-                case 1 : Data.setStartTime(System.currentTimeMillis());
-                         Calculation.start();
+                case 1 :
+                    System.out.println("helo");
+                         calculation.start();
                          break;
                 case 2 : Data.setStart(false);
                          break;
+                default:
+                    System.out.println("One not recognize...!");
             }
+
         }
+        scanner.close();
         System.out.println("-----------------------------------------------------------------------------");
         System.out.println("Thanks for using KB StopWatch....!");
         System.out.println("-----------------------------------------------------------------------------");
